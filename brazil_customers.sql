4d6ad03326615f6029116330788772b0b6b23e13
Josh Webb <joshwebb42@gmail.com>
2020-02-12 20:33:31 -0600

    brazil customer

--- brazil_customers.sql
+++ brazil_customers.sql
@@ -1,1 +1,5 @@
-﻿SELECT FirstName, LastName, CustomerId, Country FROM Customer WHERE Country='Brazil'
+﻿SELECT FirstName, LastName, CustomerId, Country 
+FROM Customer 
+WHERE Country='Brazil'
+
+--Provide a query only showing the Customers from Brazil.
