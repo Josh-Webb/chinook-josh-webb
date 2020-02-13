bbc4e8196514dda8a22334e2b8965e9f628ab7a9
Josh Webb <joshwebb42@gmail.com>
2020-02-13 16:21:21 -0600

    top agent

---
+++ top_agent.sql
@@ -0,0 +1,10 @@
+SELECT TOP 1 E.FirstName, SUM(IL.UnitPrice) as TotalAgentSales
+FROM Employee E
+JOIN Customer C on E.EmployeeId = C.SupportRepId
+JOIN Invoice I on I.CustomerId = C.CustomerId
+JOIN InvoiceLine IL on IL.InvoiceId = I.InvoiceId
+WHERE E.Title LIKE '%Sales%'
+GROUP BY E.FirstName
+ORDER BY TotalAgentSales DESC
+
+-- Which sales agent made the most in sales over all?
