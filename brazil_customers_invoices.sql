fe2d4412db1dbab9cee705aeb7a1adf06a3fc17d
Josh Webb <joshwebb42@gmail.com>
2020-02-12 20:35:24 -0600

    brazil customer invoices

--- brazil_customers_invoices.sql
+++ brazil_customers_invoices.sql
@@ -1,1 +1,8 @@
-﻿SELECT c.FirstName, c.LastName, i.InvoiceId, i.BillingCountry, i.InvoiceDate FROM Customer c LEFT JOIN Invoice i ON c.CustomerId = i.CustomerId WHERE c.Country='Brazil';
+﻿SELECT c.FirstName, c.LastName, i.InvoiceId, i.BillingCountry, i.InvoiceDate 
+FROM Customer c 
+LEFT JOIN Invoice i 
+ON c.CustomerId = i.CustomerId 
+WHERE c.Country='Brazil';
+
+/*Provide a query showing the Invoices of customers who are from Brazil. 
+The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.*/
