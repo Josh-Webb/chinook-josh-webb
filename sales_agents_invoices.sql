3389868a5284b3b1503f56f22235458c3a59b2cc
Josh Webb <joshwebb42@gmail.com>
2020-02-12 20:41:48 -0600

    sales agents invoices

--- sales_agents_invoices.sql
+++ sales_agents_invoices.sql
@@ -1,1 +1,11 @@
-﻿SELECT e.FirstName AS EmployeeFirstName, e.LastName AS EmployeeLastName, c.CustomerId, i.InvoiceId, e.EmployeeId  FROM Invoice i LEFT JOIN Customer c ON i.CustomerId = c.CustomerId LEFT JOIN Employee e ON c.SupportRepId = e.EmployeeId;
+﻿SELECT e.FirstName 
+AS EmployeeFirstName, e.LastName 
+AS EmployeeLastName, c.CustomerId, i.InvoiceId, e.EmployeeId  
+FROM Invoice i 
+LEFT JOIN Customer c 
+ON i.CustomerId = c.CustomerId 
+LEFT JOIN Employee e 
+ON c.SupportRepId = e.EmployeeId;
+
+--Provide a query that shows the invoices associated with each sales agent. 
+--The resultant table should include the Sales Agent's full name.
