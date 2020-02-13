692bd582ab39514bf1c6dcfc8e14f2f3836dd5ad
Josh Webb <joshwebb42@gmail.com>
2020-02-12 20:54:29 -0600

    line item tracks

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,1 +1,13 @@
-﻿SELECT  il.InvoiceLineId, t.Name AS TrackName, il.TrackId, al.Title AS AlbumName, ar.Name AS ArtistName FROM InvoiceLine il LEFT JOIN Track t ON il.TrackId=t.TrackId LEFT JOIN Album al ON al.AlbumId = t.AlbumId LEFT JOIN Artist ar ON ar.ArtistId = al.ArtistId;
+﻿SELECT  il.InvoiceLineId, t.Name 
+AS TrackName, il.TrackId, al.Title 
+AS AlbumName, ar.Name 
+AS ArtistName 
+FROM InvoiceLine il 
+LEFT JOIN Track t 
+ON il.TrackId=t.TrackId 
+LEFT JOIN Album al 
+ON al.AlbumId = t.AlbumId 
+LEFT JOIN Artist ar 
+ON ar.ArtistId = al.ArtistId;
+
+--Provide a query that includes the purchased track name AND artist name with each invoice line item.
