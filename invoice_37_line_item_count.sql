f0b9d6138b91e643ceb14969add15474d2efb2bf
Josh Webb <joshwebb42@gmail.com>
2020-02-12 20:51:48 -0600

    line items per invoice and invoice line 37

--- invoice_37_line_item_count.sql
+++ invoice_37_line_item_count.sql
@@ -1,1 +1,6 @@
-﻿SELECT COUNT(*)AS 'Invoice 37' FROM InvoiceLine WHERE InvoiceId=37;
+﻿SELECT COUNT(*)
+AS 'Invoice 37' 
+FROM InvoiceLine 
+WHERE InvoiceId=37;
+
+--Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
--- line_items_per_invoice.sql
+++ line_items_per_invoice.sql
@@ -1,1 +1,6 @@
-﻿SELECT COUNT(*)AS 'Invoices' FROM InvoiceLine GROUP BY InvoiceId;
+﻿SELECT COUNT(*)
+AS 'Invoices' 
+FROM InvoiceLine 
+GROUP BY InvoiceId;
+
+--Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice.
